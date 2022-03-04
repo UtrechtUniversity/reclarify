37d336c67793aa08712aec2e88f145fa28586ddc
Joey <70319311+joey4247@users.noreply.github.com>
2022-03-04 08:59:30 +0100

    Create automate.R
    skeleton framework for coding

---
+++ automate.R
@@ -0,0 +1,28 @@
+# currently in development
+# purpose: search a defined folder, stores all files in an array, and then loops the LDIR data process until all files are processed
+
+# start logfile
+
+# index folder and return all .xlsx files
+
+# write file names successively to array
+
+# begin loop
+
+# check file exists for certainty
+
+if(file.exists("C:/path/to/file/some_file.txt") == TRUE){
+  }
+  
+# feed variables into process
+
+# wait for callback from script
+
+# write confirmation to code line
+
+# loop breakout - write failure to log file
+
+# else - write no results to log file
+
+# end loop
+
