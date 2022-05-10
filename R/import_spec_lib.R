359bb45b80183e86fa235d71c560766788b28025
joey4247 <joey.odell2@gmail.com>
2022-05-10 09:23:44 +0200

    updated in-function doc and defined params explicitly

--- R/import_spec_lib.R
+++ R/import_spec_lib.R
@@ -1,7 +1,15 @@
-# spectral libraries import routine
-# imports two pre-processed libraries
-
-# currently works from the preprocessed files, needs to run from the data exported from clarity
+#' Import Spec Lib
+#'
+#' This function takes two excel spreadsheets with the spectral library output
+#' that has already been processed and combines them into one library
+#' It relies on data being preprocesesd by the earlier steps.
+#'
+#' @param lib1file Path to the first processed library as XLSX
+#' @param lib1sheet The excel sheet name to pick the data from
+#' @param lib2file Path to the second processed library as XLSX
+#' @param lib2sheet The excel sheet name to pick the data from
+#' @return A data frame containing a single particle library
+#' @export
 
 import_spec_lib <-function(lib1file, lib1sheet, lib2file, lib2sheet) {
   df.SpecLib_1 <- readxl::read_excel(lib1file,
