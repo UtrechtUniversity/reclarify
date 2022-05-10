83a52f49fbe8c92064d1e53b16bdb9650cc72935
joey4247 <joey.odell2@gmail.com>
2022-05-10 09:52:10 +0200

    tidied up code stack and added documentation

--- R/classify_spectral_data.R
+++ R/classify_spectral_data.R
@@ -4,28 +4,23 @@
 #' with a match lower than 0.8, and/or that have been rejected in the sorting
 #' sheet. It relies on data being preprocesesd by the earlier steps.
 #'
-#' @param input Path to the input file
-#' @param SizeClasses An array with the defined size class bins
-#' @return A matrix of the infile
+#' @param LDIRData Data frame containing LDIR particle analysis data
+#' @param SpecLib Data frame containing spectral family mappings
+#' @param HitRate Quality rate lower bound as decimal
+#' @param SizeClass Array of integers for size binning
+#' @return Input dataset classified by polymer family and identified as plastic or not
 #' @export
 
-classify_spectral_data <-function(input, library, output, SizeClasses, sizelabels) {
-  df.data_step1 <- dplyr::filter(df.LDIRData, Quality>=0.8)
+classify_spectral_data <-function(LDIRData, SpecLib, HitRate = 0.8, SizeClasses = c(0,10,50,100,200,500,900)) {
+  LDIRData <- dplyr::filter(LDIRData, Quality>=HitRate)
   # Filter again to remove rejected results
-  df.data_filtered <- dplyr::filter(df.data_step1, Is.Valid=="true")
+  LDIRData <- dplyr::filter(LDIRData, Is.Valid=="true")
   # joining the filtered data table with the spectra library
-  df.data_joined <- merge(df.data_filtered, df.SpecLib, by.x = "Identification", by.y = "Spectra", all.x = TRUE)
-  df.data_joined %>% group_by(Grouping)
-  
+  LDIRData <- base::merge(LDIRData, SpecLib, by.x = "Identification", by.y = "Spectra", all.x = TRUE)
+  LDIRData %>% dplyr::group_by(Grouping)
   # binning data by predetermined size classes
-  df.data_binned <- df.data_joined %>% mutate(diameter.binned = cut(Diameter..μm., breaks=SizeClasses))
-  
+  LDIRData <- LDIRData %>% dplyr::mutate(Diameter.binned = cut(Diameter..μm., breaks=SizeClasses))
   # add IsPlastic flag to enable grouping in table
-  df.data_categorised <- df.data_binned %>% mutate(IsPlastic = ifelse(Grouping!="Natural components",1,0))
-  return(df.SpecLib)
-  
+  LDIRData <- LDIRData %>% dplyr::mutate(IsPlastic = ifelse(Grouping!="Natural components",1,0))
+  return(LDIRData)
 }
-
-# defaults to set up, can be overridden
-
-SizeClasses <- c(0,10,50,100,200,500,900)
